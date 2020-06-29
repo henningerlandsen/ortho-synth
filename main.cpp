ca8e9538bbba4bb062d487ad448e6eef8213a162
Henning Erlandsen <henning.erlandsen@mojang.com>
2020-06-29 23:28:43 +0200

    Playing notes

--- main.cpp
+++ main.cpp
@@ -21,7 +21,7 @@ AudioConnection          patchCord7(ampEnv, 0, filter1, 0);
 AudioConnection          patchCord8(filter1, 0, dac, 0);
 // GUItool: end automatically generated code
 
-float notes[84] = {
+float notes[85] = {
   32.703, 34.648, 36.708, 38.891, 41.203, 43.654, 46.249, 48.999, 51.913, 55, 58.27, 61.735,
   65.406, 69.296, 73.416, 77.782, 82.407, 87.307, 92.499, 97.999, 103.826, 110, 116.541, 123.471,
   130.813, 138.591, 146.832, 155.563, 164.814, 174.614, 184.997, 195.998, 207.652, 220, 233.082, 246.942,
@@ -33,57 +33,59 @@ float notes[84] = {
 };
 
 float channels[4] = {1.0, 1.0, 1.0, 1.0};
-float main_volume = 0.3;
+float main_volume = 0.02;
+int current_note_index = 0;
 
 struct OscData {
-  float octave = 1;
+  float octave = 0;
   float tune = 0;
 };
 OscData osc[2];
 
-void setup() {
-  // Register MIDI handler
-  usbMIDI.setHandleControlChange(onControlChange);
-  
-  // Setup audio system
-  AudioMemory(20);
-  // Turn on the full voltage amplifier
-  dac.analogReference(EXTERNAL);
-  delay(50); // Give voltage time to stabilize
-  
-  // Turn on the amp
-  pinMode(5, OUTPUT);
-  digitalWrite(5, HIGH);
-  delay(10); // Allow time to wake
 
-  // Setup audio sources
-  wave1.begin(WAVEFORM_SAWTOOTH);
-  wave1.amplitude(1.0);
-  wave1.frequency(440);
-
-  wave2.begin(WAVEFORM_SQUARE);
-  wave2.amplitude(1.0);
-  wave2.frequency(220);
-
-  noise1.amplitude(1.0);
-
-  // Set default mixer level
-  updateMixerVolume();
-
-  // Setup envelopes
-  ampEnv.noteOn();
-  filterEnv.noteOn();
-
-  filter1.octaveControl(2.5);
-  dc1.amplitude(1.0); // Feed the filter control value
+float getNoteFreq(int index) {
+  if (index < 0) {
+    return notes[0];
+  } else if (index > 84) {
+    return notes[84];
+  } else {
+    return notes[index];
+  }
 }
 
-void loop() { 
-  usbMIDI.read();
-  if (millis() % 250 == 0) {
-    ampEnv.noteOn();
-    filterEnv.noteOn();
+void updateMixerVolume() {
+  for (int i=0; i < 4; i++) {
+    mixer1.gain(i, channels[i] * main_volume);
   }
+}
+
+void updateOscFreq() {
+  float osc1_freq = getNoteFreq(current_note_index + osc[0].octave * 12);
+  Serial.print("Osc 1 Hz: ");
+  Serial.println(osc1_freq);
+
+  wave1.frequency(osc1_freq);
+  wave2.frequency(getNoteFreq(current_note_index + osc[1].octave * 12) + osc[1].tune);  
+}
+
+void onNoteOn(byte channel, byte note, byte velocity) {
+  Serial.print("Note on: ");
+  Serial.println(note);
+
+  current_note_index = note - 24;
+
+  updateOscFreq();
+
+  ampEnv.noteOn();
+  filterEnv.noteOn();
+}
+
+void onNoteOff(byte channel, byte note, byte velocity) {
+  Serial.print("Note off: ");
+  Serial.println(note);
+  ampEnv.noteOff();
+  filterEnv.noteOff();
+
 }
 
 void onControlChange(byte channel, byte control, byte midi_value) {
@@ -148,23 +150,41 @@ void onControlChange(byte channel, byte control, byte midi_value) {
   }
 }
 
-void updateMixerVolume() {
-  for (int i=0; i < 4; i++) {
-    mixer1.gain(i, channels[i] * main_volume);
-  }
+void setup() {
+  Serial.begin(38400);
+  // Register MIDI handler
+  usbMIDI.setHandleControlChange(onControlChange);
+  usbMIDI.setHandleNoteOn(onNoteOn);
+  usbMIDI.setHandleNoteOff(onNoteOff);
+  // Setup audio system
+  AudioMemory(20);
+  // Turn on the full voltage amplifier
+  // dac.analogReference(EXTERNAL);
+  delay(50); // Give voltage time to stabilize
+  
+  // Turn on the amp
+  pinMode(5, OUTPUT);
+  digitalWrite(5, HIGH);
+  delay(10); // Allow time to wake
+
+  // Setup audio sources
+  wave1.begin(WAVEFORM_SAWTOOTH);
+  wave1.amplitude(1.0);
+  wave1.frequency(440);
+
+  wave2.begin(WAVEFORM_SQUARE);
+  wave2.amplitude(1.0);
+  wave2.frequency(220);
+
+  noise1.amplitude(1.0);
+
+  // Set default mixer level
+  updateMixerVolume();
+
+  filter1.octaveControl(2.5);
+  dc1.amplitude(1.0); // Feed the filter control value
 }
 
-void updateOscFreq() {
-  const float low_octave = 55;
-  float base_freq = low_octave;
-  for (int i=0; i<osc[0].octave; i++) {
-    base_freq += base_freq;
-  }
-  wave1.frequency(base_freq + osc[0].tune);
-
-  base_freq = low_octave;
-  for (int i=0; i<osc[1].octave; i++) {
-    base_freq += base_freq;
-  }
-  wave2.frequency(base_freq + osc[1].tune);
-}
+void loop() { 
+  usbMIDI.read();
+ }
